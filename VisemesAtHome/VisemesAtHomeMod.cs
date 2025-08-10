a150ccbaad0fcaa111de181bce41acbd7a0f2e41
KyuubiYoru <kyuubi.no.yoko86@gmail.com>
2025-08-10 15:52:31 +0200

    Init

---
+++ VisemesAtHome/VisemesAtHomeMod.cs
@@ -0,0 +1,19 @@
+﻿using HarmonyLib;
+using ResoniteModLoader;
+
+namespace VisemesAtHome;
+
+public class VisemesAtHomeMod : ResoniteMod
+{
+    public override string Name => "VisemesAtHome";
+    public override string Author => "KyuubiYoru";
+    public override string Version => "0.1.0";
+    public override string? Link => "https://github.com/KyuubiYoru/VisemesAtHome";
+
+
+    public override void OnEngineInit()
+    {
+        var harmony = new Harmony("dev.KyuubiYoru.VisemesAtHome");
+        harmony.PatchAll();
+    }
+}
